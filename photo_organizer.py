9ce103102ad7d16018b6c2194b822ca29842a63f
Brett Kohler <brettkohler93@gmail.com>
2023-01-28 13:36:47 -0600

    accept user input for dir path instead of command line args

--- photo_organizer.py
+++ photo_organizer.py
@@ -5,10 +5,9 @@ import sys
 
 # specify the path to the directory containing the photos
 directory = '/Users/brettkohler/Pictures'
+directory = input('Enter the directory path where your photos are located within.\nFormatted like: /Users/brettkohler/Pictures\nDirectory path: ')
 
-if len(sys.argv) == 2:
-	directory = sys.argv[1]
-
+# get all files within a given directory recursively
 def get_all_files(directory):
 	all_files = []
 	for root, _, files in os.walk(directory):
@@ -16,11 +15,16 @@ def get_all_files(directory):
 			all_files.append(os.path.join(root, file))
 	return all_files
 
+# check if the file is already in current directory
 def file_already_in_directory(file, directory):
 	file_path = os.path.abspath(file)
 	directory = os.path.abspath(directory)
 
 	return os.path.commonprefix([file_path, directory]) == directory
+
+# determine if file is an image
+def file_is_image(file):
+	return file.endswith('.jpeg') or file.endswith('.jpg') or file.endswith('.png')
 
 # get a list of all files in the directory
 files = get_all_files(directory)
@@ -29,10 +33,14 @@ if len(files) == 0:
 	print("=== The directory you have provided is empty or does not exist")
 	exit
 
+# maintain count of files moved
+count = 0
+
 # iterate through the list of files
 for file in files:
 	# check if the file is a photo (assumes jpeg file format)
-	if file.endswith('.jpeg'):
+	if file.endswith('.jpeg') or file.endswith('.jpg') or file.endswith('.png'):
+
 		# get file path and directory
 		filePath = os.path.join(directory, file)
 
@@ -57,3 +65,8 @@ for file in files:
 			
 		# move the file to the corresponding year folder
 		shutil.move(filePath, yearDir)
+
+		# increment counter
+		count += 1
+
+print("Done: Moved " + str(count) + " images")
