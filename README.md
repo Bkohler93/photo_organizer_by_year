2d1212541686d9fcb6cb2230c8b7354775c3e521
Brett Kohler <brettkohler93@gmail.com>
2023-01-28 13:19:26 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Photo Organizer
+
+## Summary
+This MacOS utility program is used to find all photos within a given directory and place each photo into a directory corresponding to what year the file was created in. If batches of photos were uploaded at one time, the creation date is likely to be a single date, and this utility won't be much help. 
---
+++ photo_organizer.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+from datetime import datetime
+
+# specify the path to the directory containing the photos
+directory = '/Users/brettkohler/Pictures'
+
+def get_all_files(directory):
+	all_files = []
+	for root, _, files in os.walk(directory):
+		for file in files:
+			all_files.append(os.path.join(root, file))
+	return all_files
+
+def file_already_in_directory(file, directory):
+	file_path = os.path.abspath(file)
+	directory = os.path.abspath(directory)
+
+	return os.path.commonprefix([file_path, directory]) == directory
+
+# get a list of all files in the directory
+files = get_all_files(directory)
+
+# iterate through the list of files
+for file in files:
+	# check if the file is a photo (assumes jpeg file format)
+	if file.endswith('.jpeg'):
+		# get file path and directory
+		filePath = os.path.join(directory, file)
+
+		# get the file's creation time
+		timestamp = os.path.getmtime(filePath)
+
+		# convert the timestamp to a datetime object
+		date = datetime.fromtimestamp(timestamp)
+		print(date)
+
+		# extract the year from the datetime object
+		year = str(date.year)
+		if year == 2012:
+			print(file)
+
+		# get the path to the correct year directory and path to file
+		yearDir = os.path.join(directory, year)
+
+		# create a folder for the current year (if it doesn't already exist)
+		if not os.path.exists(yearDir):
+			os.makedirs(yearDir)
+		elif file_already_in_directory(file, yearDir):
+			# print(file + " already exists in directory\t" + yearDir)
+			continue
+			
+		# move the file to the corresponding year folder
+		shutil.move(filePath, yearDir)
